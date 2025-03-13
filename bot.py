78f2b72aaf3c9dc14a3883cab6907c0e7b448a32
Kai-Tay <kaishengtay82@gmail.com>
2025-03-13 16:54:01 +0800

    Add sentiment analysis feature to bot with /thoughts command; updated dependencies and bot logic

--- bot.py
+++ bot.py
@@ -6,11 +6,15 @@ import os
 import boto3
 import json
 from dotenv import load_dotenv
+import requests
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Load environment variables
 load_dotenv()
 
 daysSinceDrunk = 0
+longestStreak = 0
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 # Add AWS configuration with debugging
@@ -26,14 +30,19 @@ sqs = boto3.client(
     aws_secret_access_key=AWS_SECRET_KEY,
 )
 
+# Initialize VADER sentiment analyzer
+nltk.download('vader_lexicon')
+sid = SentimentIntensityAnalyzer()
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
 # Helper functions
-def get_days_since_drunk():
+def get_info():
     daysSinceDrunk = 0
+    longestStreak = 0
     if not os.path.exists('daysSinceDrunk.txt'):
         with open('daysSinceDrunk.txt', 'w') as file:
             file.write(str(0))
@@ -58,7 +67,13 @@ async def get_qotd():
             ReceiptHandle=message['ReceiptHandle']
         )
         return quote, user
-    return "No quote sent today 🥹, send one to me with /qotd (message)!", "KaishengDrunkSohaiBot"
+    else:
+        # Retrieve the quote from api
+        response = requests.get('https://api.quotable.io/quotes/random?tags=motivational%7Csuccess')
+        body = response.json()[0]
+        user = body['author']
+        quote = body['content']
+        return quote, user
     
 
 # Scheduled message
@@ -70,7 +85,7 @@ async def send_scheduled_message(context: CallbackContext):
 
     # Send the message to the channel
     channel_username = '-1002157531667'  
-    message = f"Good Morning Everyone! I am sober for {daysSinceDrunk} days! Cheers to that! 🍻 \n\n{qotd} - {user}"
+    message = f"Good Morning Everyone! I am sober for {daysSinceDrunk} days! Cheers to that! 🍻 \n\n{qotd} - {user} \n\n Also I've added a new feature to the bot! You can now send me your thoughts (/thoughts) and watch what it replies with! 💭"
     await context.bot.send_message(chat_id=channel_username, text=message)
 
     # Update the daysSinceDrunk.txt file
@@ -83,7 +98,7 @@ async def send_scheduled_message(context: CallbackContext):
 # Commands
 async def send_drunk_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     qotd, user   = await get_qotd()
-    message = f"Good Morning Everyone! I am sober for {daysSinceDrunk} days! Cheers to that! 🍻 \n\n{qotd} - {user}"
+    message = f"Good Morning Everyone! I am sober for {daysSinceDrunk} days! Cheers to that! 🍻 \n\n Longest streak sober: {longestStreak}\n\n{qotd} - {user}"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -143,9 +158,45 @@ async def qotd(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text="Sorry, there was an error saving your quote. Please try again later."
         )
 
+async def thoughts(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Please provide a thoughts after the command. Example: /thoughts Your thoughts here"
+        )
+        return
+
+    thoughts = ' '.join(context.args)
+
+    # Use VADER to get the sentiment of the thoughts
+    sentiment = sid.polarity_scores(thoughts)
+    if sentiment['compound'] > 0.5:
+        # Give a positive message
+        sentiment_message = "You're doing great! Keep pushing forward and know that I'm always here hehe! 🔥"
+    elif sentiment['compound'] < -0.5:
+        # Give a encouragement message
+        sentiment_message = "HELLOOO Just wanted to remind you that no matter how tough things may seem right now, you’ve got the strength and resilience to push through it (and you've done it before!). Keep believing in yourselves, and don’t forget to celebrate the small victories along the way. JIAYOUS! You’ve got this! "
+    else:
+        # Give a neutral message
+        sentiment_message = "I'm not sure what you're feeling..., but can always drop me a text if you wanna talk.\n\n Keep pushing forward, and know that I'm always here to support you hehe. 🤓 💪"
+
+
+    try:
+        ## Reply to the user
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"{sentiment_message}"
+        )
+    except Exception as e:
+        logging.error(f"Error sending message to user: {str(e)}")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Sorry, there was an error reading your thoughts 😭. Please try again later."
+        )
+
 def main():
     global daysSinceDrunk
-    daysSinceDrunk = get_days_since_drunk()
+    daysSinceDrunk = get_info()
 
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
@@ -164,6 +215,10 @@ def main():
     # Add the qotd handler
     qotd_handler = CommandHandler('qotd', qotd)
     application.add_handler(qotd_handler)
+
+    # Add the thoughts handler
+    thoughts_handler = CommandHandler('thoughts', thoughts)
+    application.add_handler(thoughts_handler)
 
     application.run_polling(poll_interval=5.0)
 
